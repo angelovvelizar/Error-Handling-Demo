0d6c9aaacf40644a9d334ca8f5e39c4d4293af42
Velizar Angelov <ac0nphantom@gmail.com>
2022-07-12 20:07:58 +0300

    Initial steps.

---
+++ errors-demo/src/main/java/com/example/errorsdemo/ErrorsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.errorsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorsDemoApplication.class, args);
+	}
+
+}
---
+++ errors-demo/src/test/java/com/example/errorsdemo/ErrorsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.errorsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
